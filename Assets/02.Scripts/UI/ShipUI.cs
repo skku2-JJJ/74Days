bf9fccda36f789b7b07e7819c2016f903d3d4fde
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-24 18:18:44 +0900

    feat : 배 상태 dottween 적용

--- Assets/02.Scripts/UI/ShipUI.cs
+++ Assets/02.Scripts/UI/ShipUI.cs
@@ -1,26 +1,32 @@
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using DG.Tweening;
 
 public class ShipUI : MonoBehaviour
 {
     public int Hp;
+    private int _preHP; 
 
     [SerializeField]
     private RectTransform _water;
     void Start()
     {
-        
+        _preHP = Hp;    
     }
 
     void Update()
     {
-        ShipStateUpdate(Hp);
+        if (_preHP != Hp)
+        {
+            ShipStateUpdate(Hp);
+            _preHP = Hp;
+        }
     }
 
     void ShipStateUpdate(int hp)
     {
         Vector2 pos = _water.anchoredPosition;
         pos.y = Mathf.Clamp(-hp, -100, 0);
-        _water.anchoredPosition = pos;
+        _water.DOAnchorPos(pos, 1f).SetEase(Ease.InOutQuad); ;
     }
 }
