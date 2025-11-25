34baf1545bc17706df5f4df1de6955a004e24a5e
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-25 14:42:14 +0900

    feat : inventory ui 기본 세팅

--- Assets/03.GameAssets/Inventory System - Kinnly/Script/Inventory/Utility/TrashCan.cs
+++ Assets/03.GameAssets/Inventory System - Kinnly/Script/Inventory/Utility/TrashCan.cs
@@ -8,17 +8,14 @@ namespace Kinnly
     public class TrashCan : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] Animator animator;
-        [SerializeField] PlayerInventory playerInventory;
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            playerInventory.IsHoveringTrashcan = true;
             animator.SetBool("State", true);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            playerInventory.IsHoveringTrashcan = false;
             animator.SetBool("State", false);
         }
     }
