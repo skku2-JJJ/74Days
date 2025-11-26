7c187e2b523c0aec8a3bf6f955ec9153cc186d14
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-26 15:22:20 +0900

    feat : ship report 데이터 연동

---
+++ Assets/02.Scripts/UI/ShopFix/FixShip.cs
@@ -0,0 +1,101 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FixShip : MonoBehaviour
+{
+
+    private int _holdAmount = 10;
+    private int _fixAmount = 0;
+
+    [SerializeField]
+    private Button _minusButton;
+
+    [SerializeField]
+    private Button _plusButton;
+
+    [SerializeField]
+    private Button _fixButton;
+
+    [SerializeField]
+    private Slider _shipHealthSlider;
+
+    [SerializeField]
+    private FixAmountText _amountText;
+
+    private float _dotweenDuration = 1f;
+    void Start()
+    {
+        init();
+    }
+
+
+    public void AmountMinus()
+    {
+        _fixAmount--;
+        _amountText.AmountTextUpdate(_fixAmount);
+
+        ButtonUpdate();
+    }
+    public void AmountPlus()
+    {
+        _fixAmount++;
+        _amountText.AmountTextUpdate(_fixAmount);
+        ButtonUpdate();
+    }
+
+    public void ButtonUpdate()
+    {
+
+        holdItemUpdate();
+        if (_holdAmount == 0)
+        {
+            _minusButton.interactable = false;
+            _plusButton.interactable = false;
+        }
+        else
+        {
+            if (_fixAmount == 0)
+            {
+                _minusButton.interactable = false;
+                _fixButton.interactable = false;
+            }   
+            else
+            {
+                _fixButton.interactable = true;
+                _minusButton.interactable = true;
+            }
+                
+            if (_fixAmount == _holdAmount)
+                _plusButton.interactable = false;
+            else
+                _plusButton.interactable = true;
+        }
+    }
+
+    public void holdItemUpdate()
+    {
+        _holdAmount = ShipManager.Instance.GetResourceAmount(ResourceType.Wood);
+        Debug.Log(_holdAmount);
+    }
+
+    public void Fix()
+    {
+        ShipManager.Instance.RepairShip(_fixAmount);
+        init();
+    }
+
+    public void SliderUpdate(float shipHealth)
+    {
+        DOTween.To(() => _shipHealthSlider.value, x => _shipHealthSlider.value = x, shipHealth, _dotweenDuration);
+    }
+
+    public void init()
+    {
+        _fixAmount = 0;
+        ButtonUpdate();
+        _amountText.AmountTextUpdate(_fixAmount);
+        SliderUpdate(ShipManager.Instance.Ship.Hp);
+    }
+}
