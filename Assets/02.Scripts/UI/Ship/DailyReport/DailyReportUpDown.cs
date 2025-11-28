76103e8909efcfb28c9481fb75c9cc3568cfc50f
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-11-28 21:54:19 +0900

    fix : Dotween 꼬임 에러 fix

--- Assets/02.Scripts/UI/Ship/DailyReport/DailyReportUpDown.cs
+++ Assets/02.Scripts/UI/Ship/DailyReport/DailyReportUpDown.cs
@@ -62,6 +62,10 @@ public class DailyReportUpDown : MonoBehaviour
     public void Open()
     {
         if (_isOpen) return; // 이미 열려있으면 무시
+
+        _crewsUI.DOKill();
+        _reoportUI.DOKill();
+
         UIManager.Instance.IsOpened = true;
         _isOpen = true;
         _crewsUI.DOAnchorPos(_closePos, 0.3f).SetEase(Ease.InSine).OnComplete(() =>
@@ -73,6 +77,10 @@ public class DailyReportUpDown : MonoBehaviour
     public void Close()
     {
         if (!_isOpen) return; // 이미 닫혀있으면 무시
+
+        _crewsUI.DOKill();
+        _reoportUI.DOKill();
+
         UIManager.Instance.IsOpened = false;
         _isOpen = false;
         _reoportUI.DOAnchorPos(_closePos, 0.5f).SetEase(Ease.InBack).OnComplete(() =>
