977111f4c8f0f4caf833f820b2c4c7f367d7d23c
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-01 16:06:04 +0900

    refactor : 선원 UI 업데이트 컴포넌트 리팩토링

--- Assets/02.Scripts/UI/CrewUIUpdate.cs
+++ Assets/02.Scripts/UI/CrewUIUpdate.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CrewEmoticonUpdate : MonoBehaviour
+public class CrewUIpdate : MonoBehaviour
 {
     [SerializeField]
     private int _crewID = 0;
+    private CrewMember _crew;
 
-    [SerializeField]
-    private Image _emoticon;
+    [Header("이모티콘 변경")]
+    [SerializeField] private Image _emoticon;
+    [SerializeField] private Sprite[] _spritesEmoticon;
 
-    [SerializeField]
-    private Sprite[] Sprites;
+    [Header("죽은 선원 이미지 변경")]
+    [SerializeField] private Image _image;
+    [SerializeField] private Sprite[] _spritesImage;
     void Awake()
     {
 
@@ -20,7 +23,7 @@ public class CrewEmoticonUpdate : MonoBehaviour
             DayManager.Instance.OnDayStart += OnDayStart;
         }
     }
-    
+
     void OnDestroy()
     {
         // 이벤트 구독 해제
@@ -31,20 +34,37 @@ public class CrewEmoticonUpdate : MonoBehaviour
     }
     void Start()
     {
+        _crew = FindCrew();
         EmojiUpdate();
     }
 
     void Update()
+    {
+
+    }
+
+    void ImageUpdate()
     {
         
     }
 
     void EmojiUpdate()
     {
+       
+        if(_crew == null) { 
+            Debug.Log("해당 크루는 존재하지 않습니다.");
+            return;
+        }
+        _emoticon.sprite = _spritesEmoticon[(int)_crew.Status];
+        
+    }
+
+    CrewMember FindCrew()
+    {
         if (CrewManager.Instance == null)
         {
             Debug.LogWarning("[MorningReportUI] CrewManager.Instance가 null입니다.");
-            return;
+            return null;
         }
 
         var crewMembers = CrewManager.Instance.CrewMembers;
@@ -52,9 +72,10 @@ public class CrewEmoticonUpdate : MonoBehaviour
         foreach (var crewMember in crewMembers)
         {
             if (crewMember.CrewID != _crewID) continue;
-            Debug.Log(crewMember.CrewName);
-            _emoticon.sprite = Sprites[(int)crewMember.Status];
+
+            return crewMember;
         }
+        return null;
     }
 
     // 새 날 시작 시 호출
