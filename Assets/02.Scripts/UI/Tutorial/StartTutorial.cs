b7591b21166e2e0611cf35c5577eb66c197c1c19
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-03 15:49:30 +0900

    feat : 튜토리얼 Main Scene 적용

--- Assets/02.Scripts/UI/Tutorial/StartTutorial.cs
+++ Assets/02.Scripts/UI/Tutorial/StartTutorial.cs
@@ -7,19 +7,21 @@ public class StartTutorial : MonoBehaviour
     [SerializeField] Tutorial _tutorial;
     [SerializeField] Story _story;
 
-    private bool _isTutorialStarted = false;    
+    private bool _isTutorialStarted = false;
+    private bool _isStoryStarted = false;
 
     void Start()
     {
         _tutorial.gameObject.SetActive(false);
         _story.gameObject.SetActive(false);
-        StartIfFirstDay();
+        if (DayManager.Instance.CurrentDay == 1) _isStoryStarted = false;
     }
 
     public void StartIfFirstDay()
     {
         if (DayManager.Instance.CurrentDay == 1 && DayManager.Instance.currentPhase == DayPhase.Morning)
         {
+            _isStoryStarted = true;
             _isTutorialStarted = false;
             _story.StoryInit();
         }
@@ -27,6 +29,11 @@ public class StartTutorial : MonoBehaviour
 
     private void Update()
     {
+        
+        if (!_isStoryStarted) 
+        {
+            StartIfFirstDay();
+        }
         if (!_story.IsStoryEnd)
         {
             if (Input.GetKeyDown(KeyCode.Return))
