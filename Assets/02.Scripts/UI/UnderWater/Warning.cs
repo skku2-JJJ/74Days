9314f1fe93d0b2e5d1150df79163ac3115207dff
g1-1g <111170307+g1-1g@users.noreply.github.com>
2025-12-04 15:18:04 +0900

    refactor : warning 리팩토링

--- Assets/02.Scripts/UI/UnderWater/Warning.cs
+++ Assets/02.Scripts/UI/UnderWater/Warning.cs
@@ -6,11 +6,12 @@ public class Warning : MonoBehaviour
 {
     [SerializeField] private Image _warningHPImage; // 깜빡이는 이미지
     [SerializeField] private Image _warningO2Image; // 깜빡이는 이미지
+
     [SerializeField] private float _maxHPAlpha = 0.7f; // 체력이 0일 때의 최대 불투명도
     [SerializeField] private float _minHPAlpha = 0f;   // 체력이 충분할 때(여유) 최소 불투명도
 
-    [SerializeField] private float _maxO2Alpha = 0.7f; // 체력이 0일 때의 최대 불투명도
-    [SerializeField] private float _minO2Alpha = 0f;   // 체력이 충분할 때(여유) 최소 불투명도
+    [SerializeField] private float _maxO2Alpha = 0.7f; // 산소가 0일 때의 최대 불투명도
+    [SerializeField] private float _minO2Alpha = 0f;   // 산소가 충분할 때(여유) 최소 불투명도
     [SerializeField] private float _blinkTime = 0.5f; // 깜빡이는 속도
     [SerializeField] private DiverStatus _status;
 
@@ -25,33 +26,17 @@ public class Warning : MonoBehaviour
 
     private void Start()
     {
-        // blinkValue를 0 ↔ 1 사이에서 계속 반복시키는 Tween (딱 한 번 만들기)
-        DOTween.To(() => _blinkValue, x => _blinkValue = x, 1f, _blinkTime)
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(Ease.OutCubic);
-
-        _warningHPImage.gameObject.GetComponent<RectTransform>().DOScale(new Vector3(1.5f, 1.5f, 0f), _blinkTime)
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(Ease.OutCubic);
-        _warningO2Image.gameObject.GetComponent<RectTransform>().DOScale(new Vector3(1.5f, 1.5f, 0f), _blinkTime)
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(Ease.OutCubic);
+        TweenMake();
+        ScaleChange();
     }
 
     void Update()
     {
         UpdateHPWarning(_status.CurrentHp / _blinkHPStart);
         UpdateO2Warning(_status.CurrentOxygen / _blinkO2Start);
-        // 매 프레임 현재 hpTargetAlpha와 blinkValue 조합해서 실제 알파 적용
-        float finalHPAlpha = _blinkValue * _hpTargetAlpha;
-        float finalO2Alpha = (1-_blinkValue) * _o2TargetAlpha;
 
-        var cHP = _warningHPImage.color;
-        cHP.a = finalHPAlpha;
-        _warningHPImage.color = cHP;
-        var cO2 = _warningO2Image.color;
-        cO2.a = finalO2Alpha;
-        _warningO2Image.color = cO2;
+        AlphaUpdate(_warningHPImage, _blinkValue, _hpTargetAlpha);
+        AlphaUpdate(_warningO2Image,1 - _blinkValue, _o2TargetAlpha);
     }
 
     // HP 변화 주입
@@ -63,5 +48,33 @@ public class Warning : MonoBehaviour
     public void UpdateO2Warning(float o2Ratio)
     {
         _o2TargetAlpha = Mathf.Lerp(_maxO2Alpha, _minO2Alpha, o2Ratio);
+    }
+
+    private void TweenMake()
+    {
+        // blinkValue를 0 ↔ 1 사이에서 계속 반복시키는 Tween (딱 한 번 만들기)
+        DOTween.To(() => _blinkValue, x => _blinkValue = x, 1f, _blinkTime)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.OutCubic);
+    }
+
+    private void ScaleChange()
+    {
+        _warningHPImage.gameObject.GetComponent<RectTransform>().DOScale(new Vector3(1.5f, 1.5f, 0f), _blinkTime)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.OutCubic);
+        _warningO2Image.gameObject.GetComponent<RectTransform>().DOScale(new Vector3(1.5f, 1.5f, 0f), _blinkTime)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.OutCubic);
+    }
+
+    private void AlphaUpdate(Image warningImage, float blinkValue, float TargetAlpha)
+    {
+        // 매 프레임 현재 hpTargetAlpha와 blinkValue 조합해서 실제 알파 적용
+        float finalAlpha = blinkValue * TargetAlpha;
+
+        var c = warningImage.color;
+        c.a = finalAlpha;
+        warningImage.color = c;
     }
 }
